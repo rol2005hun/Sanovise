54a428a8ea268faf6a7092495bb1c272010f269a
Ranyák Roland Adalbert <rol2005hun@mailbox.unideb.hu>
2025-03-26 22:12:42 +0100

    build(app): api link changed to public once again :)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -141,7 +141,7 @@ const showPrivacyModal = ref(false);
 
 const submitData = async () => {
   try {
-    const response = await fetch('https://sanovise.onrender.com/', {
+    const response = await fetch('https://sanovise.onrender.com/api/advice', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(userData.value),
