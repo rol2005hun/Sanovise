5e84247697db5357a64e1eedb7b0122b0ca4e72b
Ranyák Roland Adalbert <rol2005hun@mailbox.unideb.hu>
2025-04-01 15:17:09 +0200

    style: fixed the style with google maps

--- landing/pages/contact.vue
+++ landing/pages/contact.vue
@@ -7,40 +7,42 @@
                     <a :href="'tel:' + $t('pages.contact.phoneNumber')">{{ $t('pages.contact.phoneNumber') }}</a>
                 </div>
                 <div class="info-card">
-                    <h3>{{ $t('pages.contact.address') }}</h3>
-                    <iframe style="border:0" loading="lazy" allowfullscreen
-                        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJz2N7xdwNR0cRa7C8gsfrJoQ&key=AIzaSyBvm-ltijTAkX-Ov1WH-TxtpJDzyKUrQi4"></iframe>
-                </div>
-                <div class="info-card">
                     <h3>{{ $t('pages.contact.email') }}</h3>
-                    <a :href="'mailto:' + $t('pages.contact.emailAddress', { dot: '@' })">{{ $t('pages.contact.emailAddress', { dot: '@' }) }}</a>
+                    <a :href="'mailto:' + $t('pages.contact.emailAddress', { dot: '@' })">{{
+                        $t('pages.contact.emailAddress', { dot: '@' }) }}</a>
                 </div>
             </section>
 
-            <section class="contact-form">
-                <h2>{{ $t('pages.contact.contactFormTitle') }}</h2>
+            <section class="maps-form">
+                <iframe style="border:0" loading="lazy" allowfullscreen
+                    src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJz2N7xdwNR0cRa7C8gsfrJoQ&key=AIzaSyBvm-ltijTAkX-Ov1WH-TxtpJDzyKUrQi4"></iframe>
 
-                <form action="https://formsubmit.co/botpingie@gmail.com" method="POST">
-                    <label for="name">{{ $t('pages.contact.nameLabel') }}<span class="required">*</span></label>
-                    <input id="name" name="name" type="text" :placeholder="$t('pages.contact.namePlaceholder')"
-                        required />
+                <section class="contact-form">
+                    <h2>{{ $t('pages.contact.contactFormTitle') }}</h2>
 
-                    <label for="email">{{ $t('pages.contact.emailLabel') }}<span class="required">*</span></label>
-                    <input id="email" type="email" name="email"
-                        :placeholder="$t('pages.contact.emailPlaceholder', { dot: '@' })" required />
+                    <form action="https://formsubmit.co/botpingie@gmail.com" method="POST">
+                        <label for="name">{{ $t('pages.contact.nameLabel') }}<span class="required">*</span></label>
+                        <input id="name" name="name" type="text" :placeholder="$t('pages.contact.namePlaceholder')"
+                            required />
 
-                    <label for="message">{{ $t('pages.contact.messageLabel') }}<span class="required">*</span></label>
-                    <textarea id="message" name="description" :placeholder="$t('pages.contact.messagePlaceholder')"
-                        required></textarea>
+                        <label for="email">{{ $t('pages.contact.emailLabel') }}<span class="required">*</span></label>
+                        <input id="email" type="email" name="email"
+                            :placeholder="$t('pages.contact.emailPlaceholder', { dot: '@' })" required />
 
-                    <div class="send-btn">
-                        <button type="submit">{{ $t('pages.contact.submitButton') }}</button>
-                        <div class="send-btn-labels">
-                            <input type="checkbox" name="agree" id="agree" required>
-                            <label for="agree">{{ $t('pages.contact.agreeLabel') }}</label>
+                        <label for="message">{{ $t('pages.contact.messageLabel') }}<span
+                                class="required">*</span></label>
+                        <textarea id="message" name="description" :placeholder="$t('pages.contact.messagePlaceholder')"
+                            required></textarea>
+
+                        <div class="send-btn">
+                            <button type="submit">{{ $t('pages.contact.submitButton') }}</button>
+                            <div class="send-btn-labels">
+                                <input type="checkbox" name="agree" id="agree" required>
+                                <label for="agree">{{ $t('pages.contact.agreeLabel') }}</label>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </section>
             </section>
         </main>
     </div>
